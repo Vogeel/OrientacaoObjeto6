cc63cf57697fbb0f1d8a4fa6d63bc4382efb04cf
Vogeel <vitorvogel12@gmail.com>
2022-03-08 15:41:15 -0300

    Add project files.

---
+++ OrientacaoObjeto6.ConsoleApp/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoObjeto6.ConsoleApp
+{
+    internal class Celcius
+    {
+        public double C;
+        public double conversao()
+        {
+            double ValorFinal = C * 1.8 + 32;
+            return ValorFinal;
+        }
+    }
+}
---
+++ OrientacaoObjeto6.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OrientacaoObjeto6.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Celcius Converter = new Celcius();
+            Converter.C = 22;
+            Console.WriteLine($"{Converter.C}° graus celcius é a mesma coisa que {Converter.conversao():F2}° graus em Fahrenheit");
+            Console.ReadKey();
+        }
+    }
+}
